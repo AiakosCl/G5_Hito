39a4112b06510a9cbd4103a21a54f2372a2a4d61
Gerardo Alvarez <gerardo.alvarez@outlook.com>
2024-06-07 19:42:50 -0400

    Actualización trusted

--- onlyflans/settings.py
+++ onlyflans/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','https://onlyflans.onrender.com']
-CSFR_TRUSTED_ORIGINS = ['http://127.0.0.1:8000','https://onlyflans.onrender.com']
+ALLOWED_HOSTS = ['*','https://onlyflans-wdf5.onrender.com']
+CSFR_TRUSTED_ORIGINS = ['http://127.0.0.1:8000','https://onlyflans-wdf5.onrender.com']
 
 
 
